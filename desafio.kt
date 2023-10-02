4f29a422e107cfc339074a1f6af3704a2648d734
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-02 10:34:26 -0300

    chore: adicionados dados para a execucao do codigo.

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,5 @@
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
-
+            
 data class Usuario(val nome: String, val email: String)
 
 data class ConteudoEducacional(val nome: String, val nivel: Nivel, var duracao: Int = 60)
@@ -21,11 +21,31 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
         println("Matriculados na formação $nome:")
         for(usuario in inscritos) {
             println("${usuario.nome}")
-    }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Dummy Users
+    val usuario1 = Usuario("Alice", "alice@example.com")
+    val usuario2 = Usuario("Bob", "bob@example.com")
+    val usuario3 = Usuario("Charlie", "charlie@example.com")
+
+    // Dummy Content
+    val conteudo1 = ConteudoEducacional("Curso de Kotlin Básico", Nivel.BASICO, 60)
+    val conteudo2 = ConteudoEducacional("Curso de Kotlin Avançado", Nivel.AVANCADO, 90)
+    val conteudo3 = ConteudoEducacional("Curso de Android Intermediário", Nivel.INTERMEDIARIO, 75)
+
+    // Dummy Formation with Users
+    val formacao1 = Formacao("Formação de Programação", listOf(conteudo1, conteudo2))
+    formacao1.mostrarMatriculados()
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao1.mostrarMatriculados()
+
+    val formacao2 = Formacao("Formação de Desenvolvimento Android", listOf(conteudo2, conteudo3))
+    formacao2.mostrarMatriculados()
+    formacao2.matricular(usuario2)
+    formacao2.matricular(usuario3)
+    formacao2.mostrarMatriculados()
 }
